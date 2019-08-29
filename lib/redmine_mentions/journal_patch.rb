78dc548663860bf559c7f13dcaca42585e53e93b
Marius BALTEANU <marius.balteanu@zitec.com>
2019-08-29 11:18:21 +0300

    Remove setting

--- lib/redmine_mentions/journal_patch.rb
+++ lib/redmine_mentions/journal_patch.rb
@@ -9,7 +9,7 @@ module RedmineMentions
             issue = self.journalized
             project=self.journalized.project
             users=project.users.to_a.delete_if{|u| (u.type != 'User' || u.mail.empty?)}
-            users_regex=users.collect{|u| "#{Setting.plugin_redmine_mentions['trigger']}#{u.login}"}.join('|')
+            users_regex=users.collect{|u| "@#{u.login}"}.join('|')
             regex_for_email = '\B('+users_regex+')\b'
             regex = Regexp.new(regex_for_email)
             mentioned_users = self.notes.scan(regex)
